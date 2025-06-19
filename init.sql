9811e67459938646eb9599abc9eaf80160450c24
mdavindarinaldy <153896812+mdavindarinaldy@users.noreply.github.com>
2025-06-19 15:22:15 +0700

    feat: add constraint

--- init.sql
+++ init.sql
@@ -3,9 +3,13 @@ CREATE TABLE movies (
     name VARCHAR(255) NOT NULL,
     synopsis VARCHAR(255) NOT NULL,
     release_date DATE NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
     runtime INT NOT NULL,
     poster VARCHAR(255) NOT NULL,
-    backdrop VARCHAR(255) NOT NULL
+    backdrop VARCHAR(255) NOT NULL,
+    CONSTRAINT movies_constraint CHECK(
+        LENGTH(TRIM(name))>=3
+    )
 );
 
 CREATE TABLE genres (
@@ -21,6 +25,9 @@ CREATE TABLE movie_genres (
 CREATE TABLE directors (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
+    CONSTRAINT directors_constraint CHECK(
+        LENGTH(TRIM(name))>=3
+    )
 );
 
 CREATE TABLE movie_directors (
@@ -30,7 +37,10 @@ CREATE TABLE movie_directors (
 
 CREATE TABLE casts (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL,
+    CONSTRAINT casts_constraint CHECK(
+        LENGTH(TRIM(name))>=3
+    )
 );
 
 CREATE TABLE movie_casts (
@@ -43,13 +53,18 @@ CREATE TABLE users (
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     phone_number VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    CONSTRAINT movies_constraint CHECK(
+        LENGTH(TRIM(name))>=3
+        AND
+        LENGTH(password) BETWEEN 8 AND 16
+    )
 );
 
 CREATE TABLE session (
     id SERIAL PRIMARY KEY,
     id_users INT REFERENCES users(id),
-    created_at TIMESTAMP
+    created_at TIMESTAMP DEFAULT NOW
 );
 
 CREATE TABLE transactions {
@@ -57,10 +72,14 @@ CREATE TABLE transactions {
     id_users INT REFERENCES users(id),
     id_movies INT REFERENCES movies(id),
     amount INT NOT NULL,
-    price DECIMAL(10,2) NOT NULL,
     location VARCHAR(255) NOT NULL,
     cinema VARCHAR(255) NOT NULL,
     time DATE,
+};
+
+CREATE TABLE transaction_detail {
+    id SERIAL PRIMARY KEY,
+    id_transaction INT REFERENCES transactions(id)
     seats VARCHAR(255) NOT NULL,
 };
 
